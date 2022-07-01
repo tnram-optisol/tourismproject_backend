64d140c02276a3be16b53ba54fd25844560aafca
Ramaswamy TN <ramaswamy.tn@optisolbusiness.com>
2022-07-01 20:26:16 +0530

    Testing User and Auth Module

---
+++ test/auth.test.ts
@@ -0,0 +1,72 @@
+import "mocha";
+import * as chai from "chai";
+import chaiHttp = require("chai-http");
+import * as app from "../src/index";
+import { authController } from "../src/router/authRouter";
+import { AuthController } from "../src/controller/AuthController";
+import { expect } from "chai";
+import { USER_DATA } from "../src/constants/db.constants";
+
+chai.use(chaiHttp);
+
+describe("Auth Controller imports", () => {
+  const myObj = authController;
+  it("Must have a auth controller", () => {
+    new chai.Assertion(myObj).to.be.a.instanceOf(AuthController);
+  });
+});
+
+describe("Register Check", () => {
+  const user = {
+    name: "test",
+    email: "test@abc.com",
+    password: "test123",
+    contact: "8765432109",
+    place: "london",
+    roleId: 4,
+  };
+  it("Must register a user successfully", (done) => {
+    chai
+      .request(app)
+      .post("/signup")
+      .send(user)
+      .end(function (res, err) {
+        if (err) {
+          console.log(err);
+        }
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.json;
+        done();
+      });
+  });
+});
+
+describe("Login Check", () => {
+  it("Must login & return token", (done) => {
+    chai
+      .request(app)
+      .post("/signin")
+      .send({
+        email: "test@abc.com",
+        password: "test123",
+      })
+      .end(function (err, res) {
+        expect(err).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.a.property("token");
+        done();
+
+        if (err) {
+          console.log(err);
+        }
+      });
+    afterEach(async () => {
+      const user = await USER_DATA.findOneBy({
+        email: "test@abc.com",
+      });
+      USER_DATA.remove(user).then((res) => {
+        console.log("Removed user -- test@abc.com");
+      });
+    });
+  });
+});
